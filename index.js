512ecab15005d6388cb9330d1ae3b231e53dcb1b
XamuelUG <samweko@gmail.com>
2022-03-31 19:15:40 +0300

    present

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const conn = require("./db");
 
 const app = express();
 
-const PORT = process.env.PORT || 4001;
+const PORT = process.env.PORT || 4002;
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
@@ -25,7 +25,7 @@ app.post("/api/login", (req, res) => {
         throw err;
       } else {
         if (result.length > 0) {
-          res.send(res[0]);
+          res.send(result);
         } else {
           res.send("3");
         }
@@ -69,6 +69,106 @@ app.post("/api/register", (req, res) => {
     }
   );
 });
+
+/**
+ *
+ * new book route
+ */
+app.post("/api/books/new", (req, res) => {
+  conn.query("INSERT INTO books_tbl SET ?", req.body, (err, result) => {
+    if (err) {
+      res.send("1");
+      throw err;
+    } else {
+      res.send("Ok");
+    }
+  });
+});
+
+/**
+ *
+ * get recent books
+ */
+
+app.get("/api/books/all", (req, res) => {
+  conn.query("SELECT * FROM books_tbl", (err, result) => {
+    if (err) {
+      throw err;
+    } else {
+      res.send(result);
+    }
+  });
+});
+
+/**
+ *
+ * search catalog
+ */
+app.get("/api/books/search/:search", (req, res) => {
+  conn.query(
+    `SELECT * FROM books_tbl WHERE book_title LIKE '%${req.params.search}%'`,
+    (err, result) => {
+      if (err) {
+        res.send([]);
+        throw err;
+      } else {
+        res.send(result);
+      }
+    }
+  );
+});
+
+/**
+ *
+ * search catalog
+ */
+app.get("/api/books/:id", (req, res) => {
+  conn.query(
+    `SELECT * FROM books_tbl WHERE book_id = ?`,
+    req.params.id,
+    (err, result) => {
+      console.log(result);
+      if (err) {
+        res.send([]);
+        throw err;
+      } else {
+        res.send(result);
+      }
+    }
+  );
+});
+
+/**
+ *
+ * new give away
+ *
+ */
+
+app.post("/api/books/give_away", (req, res) => {
+  conn.query("INSERT INTO give_away_tbl SET ?", req.body, (err, result) => {
+    if (err) {
+      res.send("1");
+      throw err;
+    } else {
+      res.send("Ok");
+    }
+  });
+});
+
+/**
+ *
+ * Borrowed
+ */
+app.get("/api/books/borrowed", (req, res) => {
+  conn.query("SELECT * FROM give_away_tbl", (err, result) => {
+    if (err) {
+      throw err;
+    } else {
+      res.send(result);
+    }
+  });
+});
+
 // api calls
 
 app.listen(PORT, () => {
